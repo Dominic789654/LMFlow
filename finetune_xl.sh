165875f2f61745dad1a8622fbbb190a34cccf367
Domnic789654 <xliu29@gmu.edu>
2023-04-28 23:44:30 +0800

    update 0428

--- finetune_xl.sh
+++ finetune_xl.sh
@@ -15,103 +15,95 @@ bash ./scripts/run_finetune_xl.sh ${exp_name} ${data_path} ${lr} ${bs} ${model_n
 bash ./scripts/run_chatbot_vicuna_test_HA.sh ./output_models/${exp_name} > ./chatbot_logs/${exp_name}.log 2>&1 
 
 evaluation_path=/home/xiangliu/LMFlow/data/gpt4_eval
-bash ./scripts/run_evaluation_nll_HA.sh ${exp_name} nll ${evaluation_path} ${model_name_or_path}
+bash ./scripts/run_evaluation_nll_HA.sh ${exp_name} nll ${evaluation_path} ./output_models/${exp_name}
 
 
-lr=8e-4
-bs=4
-use_lora=1
-epochs=3.5
-gradient_checkpointing=False
-gradient_accumulation_steps=4
-lora_r=128
-ds_config=configs/ds_config_zero2.json
-model_name_or_path=pinkmanlove/llama-7b-hf
-exp_name="xl_062";
-data_path="/home/xiangliu/LMFlow/data/cn_v1_text2text_end_single_round_HA"
-eval_dataset_path="/home/xiangliu/LMFlow/data/gpt4_eval"
-bash ./scripts/run_finetune_xl_lora_modules.sh ${exp_name} ${data_path} ${lr} ${bs} ${model_name_or_path} ${use_lora} ${ds_config} ${epochs} ${gradient_checkpointing} ${gradient_accumulation_steps} ${lora_r} ${eval_dataset_path}  "--master_port=10065 --num_gpus=8" 
+# lr=8e-4
+# bs=4
+# use_lora=1
+# epochs=3.5
+# gradient_checkpointing=False
+# gradient_accumulation_steps=4
+# lora_r=128
+# ds_config=configs/ds_config_zero2.json
+# model_name_or_path=pinkmanlove/llama-7b-hf
+# exp_name="xl_062";
+# data_path="/home/xiangliu/LMFlow/data/cn_v1_text2text_end_single_round_HA"
+# eval_dataset_path="/home/xiangliu/LMFlow/data/gpt4_eval"
+# bash ./scripts/run_finetune_xl_lora_modules.sh ${exp_name} ${data_path} ${lr} ${bs} ${model_name_or_path} ${use_lora} ${ds_config} ${epochs} ${gradient_checkpointing} ${gradient_accumulation_steps} ${lora_r} ${eval_dataset_path}  "--master_port=10065 --num_gpus=8" 
 
-bash ./scripts/run_chatbot_vicuna_test_HA.sh ./output_models/${exp_name} > ./chatbot_logs/${exp_name}.log 2>&1 
+# bash ./scripts/run_chatbot_vicuna_test_HA.sh ./output_models/${exp_name} > ./chatbot_logs/${exp_name}.log 2>&1 
 
-evaluation_path=/home/xiangliu/LMFlow/data/gpt4_eval
-bash ./scripts/run_evaluation_nll_HA.sh ${exp_name} nll ${evaluation_path} ${model_name_or_path}
+# evaluation_path=/home/xiangliu/LMFlow/data/gpt4_eval
+# bash ./scripts/run_evaluation_nll_HA.sh ${exp_name} nll ${evaluation_path} ./output_models/${exp_name}
 
-lr=8e-4
-bs=4
-use_lora=1
-epochs=3.5
-gradient_checkpointing=False
-gradient_accumulation_steps=2
-lora_r=32
-ds_config=configs/ds_config_zero2.json
-model_name_or_path=pinkmanlove/llama-7b-hf
-exp_name="xl_063";
-data_path="/home/xiangliu/LMFlow/data/cn_v1_text2text_end_single_round_HA"
-eval_dataset_path="/home/xiangliu/LMFlow/data/gpt4_eval"
-bash ./scripts/run_finetune_xl.sh ${exp_name} ${data_path} ${lr} ${bs} ${model_name_or_path} ${use_lora} ${ds_config} ${epochs} ${gradient_checkpointing} ${gradient_accumulation_steps} ${lora_r} ${eval_dataset_path} "--master_port=10065 --num_gpus=8" 
+# lr=8e-4
+# bs=4
+# use_lora=1
+# epochs=3.5
+# gradient_checkpointing=False
+# gradient_accumulation_steps=2
+# lora_r=32
+# ds_config=configs/ds_config_zero2.json
+# model_name_or_path=pinkmanlove/llama-7b-hf
+# exp_name="xl_063";
+# data_path="/home/xiangliu/LMFlow/data/cn_v1_text2text_end_single_round_HA"
+# eval_dataset_path="/home/xiangliu/LMFlow/data/gpt4_eval"
+# bash ./scripts/run_finetune_xl.sh ${exp_name} ${data_path} ${lr} ${bs} ${model_name_or_path} ${use_lora} ${ds_config} ${epochs} ${gradient_checkpointing} ${gradient_accumulation_steps} ${lora_r} ${eval_dataset_path} "--master_port=10065 --num_gpus=8" 
 
-bash ./scripts/run_chatbot_vicuna_test_HA.sh ./output_models/${exp_name} > ./chatbot_logs/${exp_name}.log 2>&1 
+# bash ./scripts/run_chatbot_vicuna_test_HA.sh ./output_models/${exp_name} > ./chatbot_logs/${exp_name}.log 2>&1 
 
-evaluation_path=/home/xiangliu/LMFlow/data/gpt4_eval
-bash ./scripts/run_evaluation_nll_HA.sh ${exp_name} nll ${evaluation_path} ${model_name_or_path}
+# evaluation_path=/home/xiangliu/LMFlow/data/gpt4_eval
+# bash ./scripts/run_evaluation_nll_HA.sh ${exp_name} nll ${evaluation_path} ./output_models/${exp_name}
 
-lr=8e-4
-bs=4
-use_lora=1
-epochs=3.5
-gradient_checkpointing=False
-gradient_accumulation_steps=1
-lora_r=32
-ds_config=configs/ds_config_zero2.json
-model_name_or_path=pinkmanlove/llama-7b-hf
-exp_name="xl_064";
-data_path="/home/xiangliu/LMFlow/data/cn_v1_text2text_end_single_round_HA"
-eval_dataset_path="/home/xiangliu/LMFlow/data/gpt4_eval"
-bash ./scripts/run_finetune_xl.sh ${exp_name} ${data_path} ${lr} ${bs} ${model_name_or_path} ${use_lora} ${ds_config} ${epochs} ${gradient_checkpointing} ${gradient_accumulation_steps} ${lora_r} ${eval_dataset_path}  "--master_port=10065 --num_gpus=8" 
+# lr=8e-4
+# bs=4
+# use_lora=1
+# epochs=3.5
+# gradient_checkpointing=False
+# gradient_accumulation_steps=1
+# lora_r=32
+# ds_config=configs/ds_config_zero2.json
+# model_name_or_path=pinkmanlove/llama-7b-hf
+# exp_name="xl_064";
+# data_path="/home/xiangliu/LMFlow/data/cn_v1_text2text_end_single_round_HA"
+# eval_dataset_path="/home/xiangliu/LMFlow/data/gpt4_eval"
+# bash ./scripts/run_finetune_xl.sh ${exp_name} ${data_path} ${lr} ${bs} ${model_name_or_path} ${use_lora} ${ds_config} ${epochs} ${gradient_checkpointing} ${gradient_accumulation_steps} ${lora_r} ${eval_dataset_path}  "--master_port=10065 --num_gpus=8" 
 
-bash ./scripts/run_chatbot_vicuna_test_HA.sh ./output_models/${exp_name} > ./chatbot_logs/${exp_name}.log 2>&1 
+# bash ./scripts/run_chatbot_vicuna_test_HA.sh ./output_models/${exp_name} > ./chatbot_logs/${exp_name}.log 2>&1 
 
-evaluation_path=/home/xiangliu/LMFlow/data/gpt4_eval
-bash ./scripts/run_evaluation_nll_HA.sh ${exp_name} nll ${evaluation_path} ${model_name_or_path}
+# evaluation_path=/home/xiangliu/LMFlow/data/gpt4_eval
+# bash ./scripts/run_evaluation_nll_HA.sh ${exp_name} nll ${evaluation_path} ./output_models/${exp_name}
 
-lr=8e-4
-bs=4
-use_lora=1
-epochs=3.5
-gradient_checkpointing=False
-gradient_accumulation_steps=2
-lora_r=128
-ds_config=configs/ds_config_zero2.json
-model_name_or_path=pinkmanlove/llama-7b-hf
-exp_name="xl_065";
-data_path="/home/xiangliu/LMFlow/data/cn_v1_text2text_end_single_round_HA"
-eval_dataset_path="/home/xiangliu/LMFlow/data/gpt4_eval"
-bash ./scripts/run_finetune_xl_lora_modules.sh ${exp_name} ${data_path} ${lr} ${bs} ${model_name_or_path} ${use_lora} ${ds_config} ${epochs} ${gradient_checkpointing} ${gradient_accumulation_steps} ${lora_r} ${eval_dataset_path}  "--master_port=10065 --num_gpus=8" 
+# lr=8e-4
+# bs=4
+# use_lora=1
+# epochs=3.5
+# gradient_checkpointing=False
+# gradient_accumulation_steps=2
+# lora_r=128
+# ds_config=configs/ds_config_zero2.json
+# model_name_or_path=pinkmanlove/llama-7b-hf
+# exp_name="xl_065";
+# data_path="/home/xiangliu/LMFlow/data/cn_v1_text2text_end_single_round_HA"
+# eval_dataset_path="/home/xiangliu/LMFlow/data/gpt4_eval"
+# bash ./scripts/run_finetune_xl_lora_modules.sh ${exp_name} ${data_path} ${lr} ${bs} ${model_name_or_path} ${use_lora} ${ds_config} ${epochs} ${gradient_checkpointing} ${gradient_accumulation_steps} ${lora_r} ${eval_dataset_path}  "--master_port=10065 --num_gpus=8" 
 
-bash ./scripts/run_chatbot_vicuna_test_HA.sh ./output_models/${exp_name} > ./chatbot_logs/${exp_name}.log 2>&1 
+# bash ./scripts/run_chatbot_vicuna_test_HA.sh ./output_models/${exp_name} > ./chatbot_logs/${exp_name}.log 2>&1 
 
-evaluation_path=/home/xiangliu/LMFlow/data/gpt4_eval
-bash ./scripts/run_evaluation_nll_HA.sh ${exp_name} nll ${evaluation_path} ${model_name_or_path}
+# evaluation_path=/home/xiangliu/LMFlow/data/gpt4_eval
+# bash ./scripts/run_evaluation_nll_HA.sh ${exp_name} nll ${evaluation_path} ./output_models/${exp_name}
 
-lr=8e-4
-bs=4
-use_lora=1
-epochs=3.5
-gradient_checkpointing=False
-gradient_accumulation_steps=1
-lora_r=128
-ds_config=configs/ds_config_zero2.json
-model_name_or_path=pinkmanlove/llama-7b-hf
-exp_name="xl_066";
-data_path="/home/xiangliu/LMFlow/data/cn_v1_text2text_end_single_round_HA"
-eval_dataset_path="/home/xiangliu/LMFlow/data/gpt4_eval"
-bash ./scripts/run_finetune_xl_lora_modules.sh ${exp_name} ${data_path} ${lr} ${bs} ${model_name_or_path} ${use_lora} ${ds_config} ${epochs} ${gradient_checkpointing} ${gradient_accumulation_steps} ${lora_r} ${eval_dataset_path}  "--master_port=10065 --num_gpus=8" 
+# for exp_name in 061 062
+# do 
+#     bash ./scripts/run_chatbot_vicuna_test_HA.sh ./output_models/xl_${exp_name} > ./chatbot_logs/xl_${exp_name}.log 2>&1 
+# done 
 
-bash ./scripts/run_chatbot_vicuna_test_HA.sh ./output_models/${exp_name} > ./chatbot_logs/${exp_name}.log 2>&1 
-
-evaluation_path=/home/xiangliu/LMFlow/data/gpt4_eval
-bash ./scripts/run_evaluation_nll_HA.sh ${exp_name} nll ${evaluation_path} ${model_name_or_path}
+# for exp_name in 061 062 063 064
+# do 
+#     evaluation_path=/home/xiangliu/LMFlow/data/gpt4_eval
+#     bash ./scripts/run_evaluation_nll_HA.sh xl_${exp_name} nll ${evaluation_path} ./output_models/xl_${exp_name}
+# done 
 
 # for exp_name in 027 029 039 
 # do
