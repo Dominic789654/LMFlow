1be609162955d52dd5439d829fc31b72ac75d117
domini789654 <xliu29@gmu.edu>
2024-01-04 01:58:03 +0800

    update code

--- src/lmflow/models/hf_decoder_model.py
+++ src/lmflow/models/hf_decoder_model.py
@@ -313,36 +313,36 @@ class HFDecoderModel(DecoderModel, Tunable):
                 model.print_trainable_parameters()
                 # breakpoint()
 
-                if "llama" in model_args.model_name_or_path:
-                    for name, param in model.named_parameters():
-                        # LLaMa: model.norm, model.layers.input_layernorm, model.layers.post_attention_layernorm
-                        if  "norm" in name:
-                            param.requires_grad = True        
-                        elif "lm_head" in name:
-                            param.requires_grad = True
-                        elif "embed_tokens" in name:
-                            param.requires_grad = True
-                        elif "bias" in name:
-                            param.requires_grad = True
-                        elif "lora_" in name:
-                            param.requires_grad = True
-                        else:
-                            param.requires_grad = False
-                elif "gpt2" in model_args.model_name_or_path:
-                    # gpt2 
-                    for name, param in model.named_parameters():
-                        if  "ln_" in name:
-                            param.requires_grad = True        
-                        elif "wpe" in name:
-                            param.requires_grad = True
-                        elif "lm_head" in name:
-                            param.requires_grad = True
-                        elif "wte" in name:
-                            param.requires_grad = True
-                        elif "lora_" in name:
-                            param.requires_grad = True
-                        else:
-                            param.requires_grad = False
+                # if "llama" in model_args.model_name_or_path:
+                #     for name, param in model.named_parameters():
+                #         # LLaMa: model.norm, model.layers.input_layernorm, model.layers.post_attention_layernorm
+                #         if  "norm" in name:
+                #             param.requires_grad = True        
+                #         elif "lm_head" in name:
+                #             param.requires_grad = True
+                #         elif "embed_tokens" in name:
+                #             param.requires_grad = True
+                #         elif "bias" in name:
+                #             param.requires_grad = True
+                #         elif "lora_" in name:
+                #             param.requires_grad = True
+                #         else:
+                #             param.requires_grad = False
+                # elif "gpt2" in model_args.model_name_or_path:
+                #     # gpt2 
+                #     for name, param in model.named_parameters():
+                #         if  "ln_" in name:
+                #             param.requires_grad = True        
+                #         elif "wpe" in name:
+                #             param.requires_grad = True
+                #         elif "lm_head" in name:
+                #             param.requires_grad = True
+                #         elif "wte" in name:
+                #             param.requires_grad = True
+                #         elif "lora_" in name:
+                #             param.requires_grad = True
+                #         else:
+                #             param.requires_grad = False
 
                     # if  "ln_f" in name:
                     #     param.requires_grad = True  
