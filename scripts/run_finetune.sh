e26c9955711bd4f7bd888738e8a0f5df1a44691e
dominic789654 <xliu29@gmu.edu>
2024-03-08 19:17:07 +0800

    update for 70B model

--- scripts/run_finetune.sh
+++ scripts/run_finetune.sh
@@ -5,9 +5,10 @@
 
 # Parses arguments
 model_name_or_path=gpt2
+run_name=Llama2-7b_5e-5_bsz50_lisa_n2_k20
 # dataset_path=data/alpaca/train
 dataset_path=data/gpt4_v2
-output_dir=output_models/finetune
+output_dir=output_models/${run_name}
 deepspeed_args="--master_port=11000"
 
 while [[ $# -ge 1 ]]; do
@@ -45,23 +46,24 @@ mkdir -p ${output_dir} ${log_dir}
 
 deepspeed ${deepspeed_args} \
   examples/finetune.py \
-    --model_name_or_path meta-llama/Llama-2-7b-hf \
+    --model_name_or_path /home/liuxiang/.cache/huggingface/hub/models--meta-llama--Llama-2-70b-hf/snapshots/90052941a64de02075ca800b09fcea1bdaacb939 \
     --dataset_path ${dataset_path} \
     --output_dir ${output_dir} --overwrite_output_dir \
     --num_train_epochs 1 \
     --learning_rate 5e-5 \
     --block_size 512 \
-    --per_device_train_batch_size 50 \
-    --deepspeed configs/ds_config_zero2.json \
+    --per_device_train_batch_size 1 \
+    --deepspeed configs/ds_config_zero3.json \
     --fp16 \
-    --run_name Llama2-7b_5e-5_bsz50_lisa_n2_k20 \
+    --run_name ${run_name} \
     --validation_split_percentage 0 \
     --logging_steps 10 \
     --do_train \
     --ddp_timeout 72000 \
     --save_steps 5000 \
     --dataloader_num_workers 1 \
-    --gradient_checkpointing True \
+    --gradient_checkpointing False \
+    --torch_dtype float16 \
     | tee ${log_dir}/train.log \
     2> ${log_dir}/train.err
 
